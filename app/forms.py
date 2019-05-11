4d80529125a17c0c9eec29fc16ad4770a530cf51
Maksym Hnoinskyi <mxmgny@vivaldi.net>
2019-05-11 19:09:38 +0300

    Web-service initial

---
+++ app/forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from flask_uploads import TEXT, IMAGES
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.fields.html5 import DateTimeField, TimeField, DateField
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+from app.models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+
+class FileUploadForm(FlaskForm):
+    file = FileField('filename')
+    date = DateField('date')
+    time = TimeField('time')
+    #date = DateField('Date', format='%Y-%m-%d')
+    #time = TimeField('Time')
+    submit = SubmitField("Upload File")
---
+++ app/routes.py
@@ -0,0 +1,116 @@
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, FileUploadForm
+from app.models import User, File
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+import datetime
+
+
+@app.route('/')
+@app.route('/index', methods=['GET'])
+@login_required
+def index():
+    files = File.query.filter_by(user_id=current_user.id)
+    for file in files:
+        if expired(file.datetime):
+            db.session.delete(file)
+            db.session.commit()
+    files = File.query.filter_by(user_id=current_user.id)
+    return render_template('index.html', title='', files=files)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # check if we have user already authentificated
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    # create form Object
+    form = LoginForm()
+    if form.validate_on_submit():
+        # look for username in db and get first result
+        user = User.query.filter_by(username=form.username.data).first()
+        # if no user found or password mismatch login again
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        # redirect after login to desti
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('congratulations, you\'re now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+@login_required
+def upload():
+    if current_user.is_anonymous:
+        return redirect(url_for('login'))
+    form = FileUploadForm()
+    if form.validate_on_submit():
+        file = request.files['file']
+        date = form.date.data
+        time = form.time.data
+        datetime = datetime_convert(date, time)
+        new_file = File(filename=file.filename, data=file.read(),
+                        datetime=datetime, user_id=current_user.id)
+        db.session.add(new_file)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('upload.html', title='File upload', form=form)
+
+
+@app.route('/file/<filename>/<file_id>', methods=['GET'])
+@login_required
+def file(filename, file_id):
+    if current_user.is_anonymous:
+        return redirect(url_for('login'))
+    file = File.query.filter_by(filename=filename, user_id=current_user.id, id=file_id).first()
+    if expired(file.datetime):
+        return render_template('404.html', title='404 Not Found')
+    return render_template('file.html', title=file.filename, datetime=file.datetime)
+
+
+@app.route('/404')
+def not_found():
+    return render_template('404.html', title='Error 404')
+
+
+def datetime_convert(date, time):
+    d = str(date.strftime('%Y-%m-%d')).split('-')
+    t = str(time).split(':')
+    array = d + t
+    for x in range(0, len(array)):
+        array[x] = int(array[x])
+    return datetime.datetime(*array)
+
+
+def expired(expiration_time):
+    current_time = datetime.datetime.now()
+    if expiration_time > current_time:
+        return False
+    else:
+        return True
